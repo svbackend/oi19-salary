89e5d4dd0afb5c7727f1e966a7142b76f14e64d0
Valentyn Saik <svbackend22@gmail.com>
2018-11-25 16:06:48 +0100

    Main algo + compiled version

--- salary.kt
+++ salary.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.util.Arrays
 
 fun main(args: Array<String>) {
 
@@ -6,31 +7,116 @@ fun main(args: Array<String>) {
         val startTime = System.currentTimeMillis()
 
         val inputFilename = it.toString()
-        println("---")
         val inputFileData = File(inputFilename).useLines { it.toList() }
 
         val employeesCount: Int = inputFileData[0].toInt()
-        var managers: IntArray = IntArray(employeesCount)
-        var salaries: IntArray = IntArray(employeesCount)
+        val employeesCountWithBoss = employeesCount+1
+        var managers: IntArray = IntArray(employeesCountWithBoss+1)
+        var employees: IntArray = IntArray(employeesCountWithBoss+1)
+        var salaries: IntArray = IntArray(employeesCountWithBoss)
+        var levels: IntArray = IntArray(employeesCountWithBoss+1)
+        var queue: IntArray = IntArray(employeesCountWithBoss+1)
+        var managerEmployeesCount: IntArray = IntArray(employeesCountWithBoss+1)
+        var usedSalaries: IntArray = IntArray(employeesCountWithBoss+1)
+        var startIndex: Int = 0
+        var endIndex: Int = 0
 
-        for (i in 0 until employeesCount) {
-            val rowData = inputFileData[i+1].split(" ")
+        for (i in 1..employeesCount) {
+            val rowData = inputFileData[i].split(" ")
             managers[i] = rowData[0].toInt()
             salaries[i] = rowData[1].toInt()
 
-            if (managers[i] === i) salaries[i] = employeesCount
-            if (salaries[i] > 0) managers[i] = employeesCount + 1
+            if (managers[i] === i) {
+                salaries[i] = employeesCount
+            }
+
+            if (salaries[i] > 0) {
+                managers[i] = employeesCountWithBoss
+            }
         }
 
-        println("Managers:")
-        println(managers.toString())
+        for (i in 1 until employeesCountWithBoss) {
+            ++levels[managers[i]]
+        }
 
-        println("Salaries:")
-        println(salaries.toString())
+        for (i in 1 until employeesCountWithBoss) {
+            if (levels[i] == 0) {
+                queue[endIndex++] = i;
+            }
+        }
+
+        while (startIndex < endIndex) {
+            val currentIndex = queue[startIndex++]
+            val manager = managers[currentIndex]
+            if (salaries[currentIndex] === 0) {
+                if (--levels[manager] == 0) {
+                    queue[endIndex++] = manager
+                }
+                managerEmployeesCount[manager] += managerEmployeesCount[currentIndex] + 1
+            }
+        }
+
+        for (i in 1 until employeesCountWithBoss) {
+            if (salaries[i] > 0) {
+                usedSalaries[salaries[i]] = i
+            } else if (employees[managers[i]] == 0) {
+                employees[managers[i]] = i
+            } else {
+                employees[managers[i]] = -1
+            }
+        }
+
+        var i = 0
+        var freePlacesCount = 0
+        var availablePlacesCount = 0
+        while (i < employeesCount) {
+            while (i < employeesCount && usedSalaries[i + 1] === 0) {
+                ++i
+                ++freePlacesCount
+                ++availablePlacesCount
+            }
+            while (i < employeesCount && usedSalaries[i + 1] !== 0) {
+                ++i
+                var akt = usedSalaries[i]
+                var l = i
+                freePlacesCount -= managerEmployeesCount[akt]
+                if (freePlacesCount == 0) {
+                    while (availablePlacesCount-- > 0 && employees[akt] > 0) {
+                        while (usedSalaries[l] > 0) --l
+                        akt = employees[akt]
+                        salaries[akt] = l
+                        usedSalaries[l] = akt
+                    }
+                    availablePlacesCount = 0
+                }
+                if (managerEmployeesCount[akt] !== 0)
+                    availablePlacesCount = 0
+            }
+        }
 
         val endTime = System.currentTimeMillis()
         val outputFileData = File(inputFilename.replace("in", "out")).useLines { it.toList() }
-        //val outputSteps = outputFileData[0].toLong()
+        var isResultCorrect = true
+        for (i in 1 until salaries.size) {
+            if (salaries[i] != outputFileData[i-1].toInt()) {
+                isResultCorrect = false
+            }
+        }
+        if (isResultCorrect === true) {
+            println("$inputFilename test passed!")
+            if (salaries.size <= 50) {
+                println("Result:")
+                println(Arrays.toString(salaries))
+            }
+
+        } else {
+            println("$inputFilename result not correct!")
+            println("Actual:")
+            println(Arrays.toString(salaries))
+            println("Expected:")
+            println(outputFileData)
+            return
+        }
 
     }
 }
